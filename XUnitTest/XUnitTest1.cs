f5dd6301477ab87043ed2a04e806a13bd8d42361
Salil Anthony Dhara <salil.dhara@finomial.com>
2018-09-06 13:42:15 +0530

    introduced Theory/InlineData attributes

--- XUnitTest/XUnitTest1.cs
+++ XUnitTest/XUnitTest1.cs
@@ -6,20 +6,17 @@ namespace XUnitTest
 {
     public class XUnitTest1
     {
-        [Fact]
-        public void PassContainsQMark()
+        [Theory]
+        [InlineData("ques")]
+        [InlineData("ques?")]
+        public void PassContainsQMark(string ques)
         {
             string[] ops = { "qw", "we", "er", "rt" };
-            var obj = new Question("ques ?", ops);
+            var obj = new Question(ques, ops);
             Assert.Contains("?",obj.question);
         }
 
-        [Fact]
-        public void FailContainsQMark()
-        {
-            string[] ops = { "qw", "we", "er", "rt" };
-            var obj = new Question("ques", ops);
-            Assert.DoesNotContain("?", obj.question);
-        }
+
+        
     }
 }
